8d4042b4045ae815c7c23a717901eeecea435f54
iventurefactory <info@iventurefactory.com>
2013-07-11 19:36:09 +0000

    enhance web.js

--- web.js
+++ web.js
@@ -3,7 +3,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+);
+
 });
 
 
